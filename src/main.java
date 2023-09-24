07155f805821f7d9c7e25a5c7f014ca3ecaff460
PhiPhiPham <Stevenpham0705@gmail.com>
2023-09-24 20:11:00 +1000

    adding the main functions for sub_scenario_2

--- src/main.java
+++ src/main.java
@@ -2,7 +2,48 @@ import sub_scenario_1.*;
 import sub_scenario_2.*;
 public class main {
     public static void main(String[] args) {
-        //sub scenario #1
+        // Instantiating a database
+        sub_scenario_1.Database database = new sub_scenario_1.Database();
+
+        //sub scenario #1 - Instructor registers a course on the learning portal/website
+
+        //// Main route of registering a new Instructor ////
+        // User wants to add register by adding a course 
+        sub_scenario_1.Course course1 = new sub_scenario_1.Course("Introduction to Dynamic Programming", "Description of Course", "Dynamic Programming");
+        
+        // Payment
+        Payment payment1 = new Payment(50, false, "paypal");
+
+        // Checking if payment is paid and pay item
+        payment1.payItem();
+
+        // Creating a new account
+        sub_scenario_1.Instructor instructor = new sub_scenario_1.Instructor("InstructorName", "InstructorAddress", "InstructorEmail", 123456789, "InstructorUsername", "InstructorPassword");
+
+        // Add Instructor to database
+        database.registerInstructor(instructor);
+
+        // Add Course to Instructor
+        instructor.addCourse(course1);
+
+        // Add Course to database;
+        database.registerCourse(course1);
+
+
+        // Alt Route of adding a course but is an existing instructor
+
+        // User wants to add register by adding a course
+        sub_scenario_1.Course course2 = new sub_scenario_1.Course("Introduction to Algorithms", "Description of Course", "Algorithms");
+
+        // Checking if payment is paid and pay item
+        payment1.payItem();
+
+        // Item has already been paid so just add course2
+        instructor.addCourse(course2);
+
+        // Add Course to database;
+        database.registerCourse(course2);
+
 
 
         //sub scenario #2 - Lodge a general enquiry about the portal which is successfully addressed by the Manager
--- src/sub_scenario_1/Instructor.java
+++ src/sub_scenario_1/Instructor.java
@@ -1,6 +1,9 @@
 package sub_scenario_1;
+import java.util.List;
 
 public class Instructor extends Admin {
+    
+    private List<Course> coursesTaught;
     
     public Instructor(String name, String address, String email, int phoneNo, String username, String password) {
         super(name, address, email, phoneNo, username, password);
@@ -8,6 +11,14 @@ public class Instructor extends Admin {
 
     public boolean accessInstructor() {
         return true;
+    }
+
+    public void addCourse(Course course) {
+        coursesTaught.add(course);
+    }
+
+    public void displayCoursesTaught() {
+        System.out.print(coursesTaught);
     }
 
 }
